eb78375ffdbc60fab1a106aa384319914545eaec
Ona-kri4ala-xvatit <live4ever4@gmail.com>
2023-12-14 11:04:13 +0400

    SecondSonCommit

--- ConsoleAppGitBranches/Program.cs
+++ ConsoleAppGitBranches/Program.cs
@@ -1,3 +1,4 @@
 ﻿// See https://aka.ms/new-console-template for more information
 Console.WriteLine("Hello, World!");
 Console.WriteLine("Hello, First son of Brad Pitt!");
+Console.WriteLine("Hello, Second son of Brad Pitt!");
